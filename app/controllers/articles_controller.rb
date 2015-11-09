86af402a8efeab78b8d2fb317a8220fd4dfc655f
Lauren Grant <dangerlauren@gmail.com>
2015-11-09 16:52:20 -0600

    getting started on the 3 person pizza club site

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,24 @@
+class ArticlesController < ApplicationController
+	def index
+		@article = Article.all
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def new
+	end
+
+	def create
+		@article = Article.new(article_params)
+
+		@article.save
+		redirect_to @article
+	end
+
+	private
+		def article_params
+			params.require(:article).permit(:author, :date, :headline, :text)
+		end
+end
