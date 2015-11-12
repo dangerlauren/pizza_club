4126f614efae3df980d70db1f2f19e0b90685857
Lauren Grant <dangerlauren@gmail.com>
2015-11-11 21:16:20 -0600

    added logout/edit to all admin pages, redirect signed in admin to review pages, styled forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    new_article_path
+  end
+
   protected
   	def configure_permitted_parameters
   		devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password) }
