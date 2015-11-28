7c60c1fb6ecb35e23ec81b1104b2d50fb8e8168c
Lauren Grant <dangerlauren@gmail.com>
2015-11-28 16:00:00 -0600

    have to be signed in to edit map

--- app/controllers/bestof_controller.rb
+++ app/controllers/bestof_controller.rb
@@ -1,5 +1,11 @@
 class BestofController < ApplicationController
 	def index
+		@pizzas = Pizza.all
+    @hash = Gmaps4rails.build_markers(@pizzas) do |pizza, marker|
+      marker.lat pizza.latitude
+      marker.lng pizza.longitude
+      marker.title pizza.title
+    end
 	end
 
 	def show
